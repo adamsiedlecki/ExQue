168bbb38614c03980fcfcac5fcab383b013937ad
adam <adamsiedlec@wp.pl>
2020-07-01 12:36:35 +0200

    Initial commit

---
+++ src/main/java/pl/adamsiedlecki/ExQue/ExQueApplication.java
@@ -0,0 +1,13 @@
+package pl.adamsiedlecki.ExQue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExQueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExQueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adamsiedlecki/ExQue/ExQueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adamsiedlecki.ExQue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExQueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
