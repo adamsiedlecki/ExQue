e2d197a3d703748751ae11d861b4819bd78e6ee9
adam <adamsiedlec@wp.pl>
2021-05-20 11:24:26 +0200

    move swagger config and provided default json

--- src/main/java/pl/adamsiedlecki/ExQue/config/SwaggerConfig.java
+++ src/main/java/pl/adamsiedlecki/ExQue/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package pl.adamsiedlecki.ExQue.swagger;
+package pl.adamsiedlecki.ExQue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/pl/adamsiedlecki/ExQue/config/WebConfig.java
@@ -0,0 +1,18 @@
+package pl.adamsiedlecki.ExQue.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(true)
+                .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+}
